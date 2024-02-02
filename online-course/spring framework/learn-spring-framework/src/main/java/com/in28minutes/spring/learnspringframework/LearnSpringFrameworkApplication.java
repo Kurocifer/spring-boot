aa7c73828771cc35cc247aee23f7561622ed4c68
Cifer-ti <draculemihawk232@gmail.com>
2024-02-02 19:29:44 +0100

    reduce coupling
    Reduce tight coupling to loose coupling using spring

--- online-course/spring framework/learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ online-course/spring framework/learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -3,29 +3,29 @@ package com.in28minutes.spring.learnspringframework;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.yaml.snakeyaml.extensions.compactnotation.PackageCompactConstructor;
 
 import com.in28minutes.spring.learnspringframework.game.GameRunner;
+import com.in28minutes.spring.learnspringframework.game.GamingConsole;
 import com.in28minutes.spring.learnspringframework.game.MarioGame;
-import com.in28minutes.spring.learnspringframework.game.SuperContraGame;
-import com.in28minutes.spring.learnspringframework.game.PacmanGame;
 
 @SpringBootApplication
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(LearnSpringFrameworkApplication.class, args);
-		MarioGame mario = new MarioGame();
-		SuperContraGame game2 = new SuperContraGame();
-		PacmanGame pacman = new PacmanGame();
+		ConfigurableApplicationContext context
+			= SpringApplication.run(LearnSpringFrameworkApplication.class, args);
 		
-		GameRunner runner = new GameRunner(mario);
-		GameRunner runner2 = new GameRunner(game2);
-		GameRunner runner3 = new GameRunner(pacman);
+		//MarioGame game = new MarioGame();
+		//SuperContraGame game1 = new SuperContraGame();
+		
+		//GameRunner runner = new GameRunner(game);
+		//GameRunner runner1 = new GameRunner(game1);
+		
+		GameRunner runner = context.getBean(GameRunner.class);
 		
 		runner.run();
-		runner2.run();
-		runner3.run();
 		
 	}
 }
