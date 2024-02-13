47ba57d45fea629e6d7127f3cb509b4b10e00473
Cifer-ti <draculemihawk232@gmail.com>
2024-02-13 21:20:40 +0100

    RESTful web services

--- online-course/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/steponmoon/StepOnMoon.java
+++ online-course/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/steponmoon/StepOnMoon.java
@@ -1,14 +1,24 @@
 package com.in28minutes.rest.webservices.restfulwebservices.steponmoon;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StepOnMoon {
 
-	@RequestMapping(method = RequestMethod.GET, path = "/step-on-moon")
+	@GetMapping(path = "/stepOn-moon")
 	public String stepOnMoon() {
 		return "A little but great step";
+	}
+	
+	@GetMapping(path = "/stepOn-moon-bean")
+	public StepOnMoonBean stepOnMoonBean() {
+		return new StepOnMoonBean("A little but great step");
+	}
+	
+	@GetMapping(path = "/stepOn-moon/path-variable/{name}")
+	public StepOnMoonBean stepOnMoonPathVariable(@PathVariable String name) {
+		return new StepOnMoonBean("A little but great step for " + name);
 	}
 }
