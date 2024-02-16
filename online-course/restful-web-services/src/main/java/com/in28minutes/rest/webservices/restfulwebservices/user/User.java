1b421084c2b536855ae420255aee8dd974467db1
Cifer-ti <draculemihawk232@gmail.com>
2024-02-16 23:06:14 +0100

    innitial commit

--- online-course/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ online-course/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -3,7 +3,7 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.time.LocalDate;
 
 public class User {
-	private int id;
+	private int id; 
 	private String name;
 	private LocalDate birthDate;
 
