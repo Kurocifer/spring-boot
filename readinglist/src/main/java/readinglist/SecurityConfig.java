3f7df8ff3259eea666ed86fc9057f7892af05152
Cifer-ti <draculemihawk232@gmail.com>
2024-02-17 23:35:04 +0100

    Add basic security for readinglist service

--- readinglist/src/main/java/readinglist/SecurityConfig.java
+++ readinglist/src/main/java/readinglist/SecurityConfig.java
@@ -5,34 +5,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+public class SecurityConfig extends  WebSecurityConfiguration {
+	
 	@Autowired
 	private ReaderRepository readerRepository;
 	
-	@Override
+	@SuppressWarnings("deprecation")
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").access("hasRole('READER')").antMatchers("/**").permitAll()
-
-				.and()
-
-				.fromLoging().logingPage("/login").failureUrl("/login?error=true");
+		http
+			.authorizeRequests()
+				.requestMatchers("/").access("hasRole('READER')")
+				.requestMatchers("/**").permitAll()
+				
+			.and()
+			
 	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(new UserDetailsService() {
-			@Override
-			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-				return readerRepository.findOne(username);
-			}
-		});
+	
+	protected void configure(
+			AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.userDetailsService(new UserDetailsService() {
+				@Override
+				public UserDetails loadUserByUsername(String username)
+					throws UsernameNotFoundException {
+					return readerRepository.findOne(username);
+				}
+			})
 	}
-
+	
 }
