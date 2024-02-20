8d58fb73afef8befea5afdd40f56d67c79eed66b
Cifer-ti <draculemihawk232@gmail.com>
2024-02-20 22:11:42 +0100

    Add verification for valid input

--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -3,9 +3,16 @@ package tacos;
 import java.util.List;
 import java.util.Objects;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Taco {
 
+	@NotNull
+	@Size(min=5, message="Name must be atleast 5 characters long")
 	private String name;
+	
+	@Size(min=3, message="You must choose at least 1 ingredient")
 	private List<String> ingredients;
 
 	public String getName() {
--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -59,7 +58,7 @@ public class DesignTacoController {
 	}
 	
 	 @PostMapping
-	  public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+	  public String processDesign(@Valid Taco design, Errors errors) {
 	    
 		 if (errors.hasErrors()) {
 	      return "design";
